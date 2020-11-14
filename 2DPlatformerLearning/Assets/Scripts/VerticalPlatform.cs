018a42bb2c1d96d000dbfe3a42f7fc37b62ab481
GreyZar37 <52458016+GreyZar37@users.noreply.github.com>
2020-11-14 18:12:54 +0100

    h

--- 2DPlatformerLearning/Assets/Scripts/VerticalPlatform.cs
+++ 2DPlatformerLearning/Assets/Scripts/VerticalPlatform.cs
@@ -8,24 +8,53 @@ public class VerticalPlatform : MonoBehaviour
     private PlatformEffector2D effector;
     public float waitTime;
 
+    float cooldownTimer = 0.2f;
+
+
+    private bool setLock;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
         effector = GetComponent<PlatformEffector2D>();
+
+        StartCoroutine(enumerator());
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        print(waitTime);
+
+     
 
         if (Input.GetKeyUp(KeyCode.S))
         {
-      
+
             waitTime = 0.2f;
 
+            setLock = true;
+            cooldownTimer =  0.2f;
+         
         }
+
+        if(setLock == true)
+        {
+            if (effector.rotationalOffset == 180f)
+            {
+                print(cooldownTimer);
+                if (cooldownTimer <= 0)
+                {
+                    effector.rotationalOffset = 0f;
+
+                }
+                cooldownTimer -= Time.deltaTime;
+            }
+
+        }
+
 
 
         if (Input.GetKey(KeyCode.S))
@@ -33,21 +62,41 @@ public class VerticalPlatform : MonoBehaviour
             if(waitTime<= 0)
             {
                 effector.rotationalOffset = 180f;
+
                 waitTime = 0.2f;
+
+
+
             }
             else
             {
                 waitTime -= Time.deltaTime;
             }
 
+            setLock = false;
 
         }
         if (Input.GetKey(KeyCode.Space))
         {
             effector.rotationalOffset = 0f;
+           
         }
       
+       
 
       
     }
+
+
+    IEnumerator enumerator()
+    {
+
+        yield return new WaitForSeconds(0.3f);
+        effector.rotationalOffset = 0f;
+
+
+        yield return null;
+    }
+
+
 }
